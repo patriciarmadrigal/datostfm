bb96974fb8de461601f297e2faa9dfaf01e8a947
Patricia <patriciarmadrigal1@gmail.com>
2020-07-10 12:36:32 +0200

    daily crontab backup Fri 10 Jul 2020 12:36:32 PM CEST

---
+++ sensor1.py
@@ -0,0 +1,33 @@
+#Libraries
+import Adafruit_DHT as dht
+import time
+from time import sleep
+import os.path
+
+lastOpennedFile = ""
+path = "/home/pi/git/TFMPatri/o1.txt"
+
+if os.path.isfile(path):
+  lastOpennedFile = open(path, "a")
+else:
+  lastOpennedFile = open(path, "w+")
+  lastOpennedFile.close()
+  lastOpennedFile = open(path, "a")
+
+def now():
+    return time.strftime('[%d-%m-%Y %H:%M:%S]')
+
+def append2File(s): 
+        lastOpennedFile.write(s+"\n")
+        lastOpennedFile.flush()
+
+#Set DATA pin
+DHT_1 = 4
+while True:
+    try: 
+        h,t = dht.read_retry(dht.DHT22, DHT_1)
+    except:
+        print("Error in sensor 1")
+    
+    append2File("{0} [S1], {1:0.01f}, {2:0.01f}".format(now(), t, h))
+    sleep(10)
